af17059f1871ae0077739f16da856b79c118cb77
錬太郎 <@LAPTOP-48KM1V7U>
2025-11-15 14:18:42 +0900

    ４週目課題追加

--- src/main/java/product/ProductsManager.java
+++ src/main/java/product/ProductsManager.java
@@ -4,11 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProductsManager {
-	//プロジェクトを格納
-	//private List<Product> productList = new ArrayList<>();
-	//プロジェクトを追加
-	//public void addProduct(Product product) {
-	//productMap.put(product.getId(), product);
+
 	private List<Product> products;
 
 	public ProductsManager() {
@@ -49,45 +45,3 @@ public class ProductsManager {
 	}
 
 }
-
-//public void addProduct(Product product) {
-//productList.add(product);
-//System.out.println("プロジェクトを追加しました："+ product.getName());	
-//}
-//IDで検索
-//public Product findProductById(int id) {
-//for (Product product : productList) {
-//if (product.getId() == id) {
-//return product;
-//}
-//}
-//return null;
-//}
-
-//public boolean removeProduct(int id) {
-//Product productRemove = findProductById(id);
-//if(productRemove != null) {
-//productList.remove(productRemove);
-//
-//return true;
-//} else {
-//
-//return false;
-//プロジェクトを削除
-
-//}
-//}
-//public void displayAllProduct() {
-//if(productList.isEmpty()) {
-//return;
-//}
-//System.out.println("全てを表示");
-//for (Product product : productList) {
-//System.out.println(product);
-
-//	}
-//System.out.println("------");
-
-//}
-
-//}
