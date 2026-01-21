0bdd70d7f90253297bf80868e4703e7474f404d9
錬太郎 <@LAPTOP-48KM1V7U>
2026-01-21 21:55:40 +0900

    9週目課題

--- src/main/java/db/PracticeDB.java
+++ src/main/java/db/PracticeDB.java
@@ -10,10 +10,11 @@ import java.util.Scanner;
 
 //データべースに接続
 public class PracticeDB {
+	//メインでしか使えないのを全体で使えるようにプライベートにした
 	private static final String url = "jdbc:mysql://localhost:3306/product_management";
 	private static final String user = "root";
 	private static final String password = "rentaro0701";
-	
+
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
@@ -57,9 +58,10 @@ public class PracticeDB {
 	}
 
 	//データ追加
+
 	public static void INSERTp() {
 		Scanner scanner = new Scanner(System.in);
-
+		System.out.print("--商品の登録--");
 		System.out.print("商品名を入力してください: ");
 		String name = scanner.nextLine();
 
@@ -80,11 +82,12 @@ public class PracticeDB {
 			pstmt.setInt(3, stock);
 			pstmt.setInt(4, category_id);
 
-			 //pstmt.executeUpdate();これが二個あったから重複して登録されていた
-			
+			//pstmt.executeUpdate();
+
 			int count = pstmt.executeUpdate();
 			System.out.println("登録成功件数：" + count + "件");
-			System.out.println("登録内容："+ "商品名：" + name + ", 価格：" + price + ", 在庫数：" + stock + ",  カテゴリーID：" + category_id);
+			System.out.println(
+					"登録内容：" + "商品名：" + name + ", 価格：" + price + ", 在庫数：" + stock + ",  カテゴリーID：" + category_id);
 		} catch (SQLException e) {
 			System.out.println("データの追加に失敗しました");
 			e.printStackTrace();
@@ -95,36 +98,87 @@ public class PracticeDB {
 	public static void UPDATEp() {
 		Scanner scanner = new Scanner(System.in);
 
-		System.out.print("商品IDを入力してください: ");
-		int id = scanner.nextInt();
-		System.out.print("価格を入力してください: ");
-		int price = scanner.nextInt();
-		System.out.print("在庫数を入力してください: ");
-		int stock = scanner.nextInt();
-//stockの後にコンマがあったことが原因
-		String sql = "UPDATE products SET price = ?, stock = ? WHERE id = ?";
-		try (Connection connection = DriverManager.getConnection(url, user, password);
-				PreparedStatement pstmt = connection.prepareStatement(sql)) {
-			pstmt.setInt(1, price);
-			pstmt.setInt(2, stock);
-			pstmt.setInt(3, id);
+		int count1 = 0;
+		int count2 = 0;
+		try (Connection connection = DriverManager.getConnection(url, user, password)) {
+			connection.setAutoCommit(false);
+			try {
+				System.out.println("--商品の更新➀--");
+				System.out.print("商品IDを入力してください: ");
+				int id1 = scanner.nextInt();
+				System.out.print("価格を入力してください: ");
+				int price1 = scanner.nextInt();
+				System.out.print("在庫数を入力してください: ");
+				int stock1 = scanner.nextInt();
+				String sql1 = "UPDATE products SET price = ?, stock = ? WHERE id = ?";
+				try (PreparedStatement pstmt1 = connection.prepareStatement(sql1)) {
+					pstmt1.setInt(1, price1);
+					pstmt1.setInt(2, stock1);
+					pstmt1.setInt(3, id1);
 
-			//pstmt.executeUpdate();
+					//int count1 = pstmt1.executeUpdate();
+					count1 = pstmt1.executeUpdate();
+					//例外が発生していないからロールバックしない
 
-			int count = pstmt.executeUpdate();
-			System.out.println("登録成功件数：" + count + "件");
-			System.out.println("登録内容; " + "商品ID;　" + id + ", 価格: " + price + ", 在庫数: " + stock);
+					//if (count1 == 0) {
+					//throw new SQLException();
+					//}
+					//1の方でエラーが出るとその段階でスローされてロールバックされる
+
+				}
+				System.out.println("--商品の更新②--");
+				System.out.print("商品IDを入力してください: ");
+				int id2 = scanner.nextInt();
+				System.out.print("価格を入力してください: ");
+				int price2 = scanner.nextInt();
+				System.out.print("在庫数を入力してください: ");
+				int stock2 = scanner.nextInt();
+				String sql2 = "UPDATE products SET price = ?, stock = ? WHERE id = ?";
+				try (PreparedStatement pstmt2 = connection.prepareStatement(sql2)) {
+					pstmt2.setInt(1, price2);
+					pstmt2.setInt(2, stock2);
+					pstmt2.setInt(3, id2);
+
+					//int count2 = pstmt2.executeUpdate();
+
+					count2 = pstmt2.executeUpdate();
+					//if (count2 == 0) {
+					//throw new SQLException();
+					//}
+				}
+				//例外を飛ばす
+				if (count1 == 0) {
+					throw new SQLException();
+				}
+				if (count2 == 0) {
+					throw new SQLException();
+				}
+				connection.commit();
+				System.out.println("コミット成功");
+				//件数数え　countを最初に定義してそこにカウントするようにした
+				//中から外に出力から外に中の情報が入るようにした
+				int count3 = count1 + count2;
+				System.out.println("登録成功件数：" + count3 + "件");
+				//変更内容表示
+				System.out.println("更新内容➀; " + "商品ID;" + id1 + ", 価格: " + price1 + ", 在庫数: " + stock1);
+				System.out.println("更新内容②; " + "商品ID;" + id2 + ", 価格: " + price2 + ", 在庫数: " + stock2);
+			} catch (SQLException e) {
+				//例外が発生していないからロールバックしない
+				connection.rollback();
+				int count3 = count1 + count2;
+				System.out.println("登録成功件数：" + count3 + "件");
+				System.out.println("ロールバックしました");
+				e.printStackTrace();
+			}
+
 		} catch (SQLException e) {
-			System.out.println("更新失敗");
-			e.printStackTrace();
+			System.out.println("データベース接続失敗");
 		}
-		
 	}
-
 	//データ削除
 	public static void DELETEp() {
 		Scanner scanner = new Scanner(System.in);
-
+		System.out.print("---商品を削除(カテゴリーID指定)--");
 		System.out.print("削除するカテゴリーIDを入力してください: ");
 		int category_id = scanner.nextInt();
 
@@ -141,6 +195,7 @@ public class PracticeDB {
 			System.out.println("データの削除に失敗しました");
 			e.printStackTrace();
 		}
-		
+
 	}
 }
+
